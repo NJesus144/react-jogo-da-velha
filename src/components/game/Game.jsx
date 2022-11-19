6364f678592f98bb9173a253a71d3866bf44d208
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-11-19 19:35:25 -0300

    condição de empate

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -20,6 +20,7 @@ function Game() {
   const [currentPlayer, setCurrentPlayer] = useState(-1);
   const [winner, setWinner] = useState(0);
   const [winnerLine, setWinnerLine] = useState([]);
+  const [draw, setDraw] = useState(false);
 
   const handleClick = (position) => {
     if (gameState[position] === 0 && winner === 0) {
@@ -44,15 +45,28 @@ function Game() {
     setGameState(Array(9).fill(0));
     setWinner(0);
     setWinnerLine([]);
+    setDraw(false)
   };
+
+  const verifyDraw = () => {
+    // if(gameState.filter((value) => value === 0).length === 0)
+    if(gameState.find((value) => value === 0) === undefined && winner === 0) {
+      setDraw(true)
+    }
+  }
 
   const verifyWinnerLine = (pos) => 
   winnerLine.find((value) => value === pos) !== undefined;
 
   useEffect(() => {
     setCurrentPlayer(currentPlayer * -1);
+    verifyDraw()
     verifyGame();
   }, [gameState]);
+
+  useEffect(() => {
+    if(winner !== 0) setDraw(false);
+}, [winner])
 
   return (
     <div className={styles.gameContent}>
@@ -63,6 +77,7 @@ function Game() {
             status={value}
             onClick={() => handleClick(position)}
             isWinner={verifyWinnerLine(position)}
+            isDraw={draw}
           />
         ))}
       </div>
@@ -70,6 +85,7 @@ function Game() {
         currentPlayer={currentPlayer}
         winner={winner}
         onReset={handleReset}
+        isDraw={draw}
       />
     </div>
   );
--- src/components/gameInfo/GameInfo.jsx
+++ src/components/gameInfo/GameInfo.jsx
@@ -2,30 +2,30 @@ import styles from "./GameInfo.module.css";
 import Icon from "../icon/Icon";
 import Button from "../button/Button";
 
-function GameInfo({ currentPlayer, winner, onReset }) {
-  const shouldEnabledButton = () => {
-    return winner !== 0;
-  };
+function GameInfo({ currentPlayer, winner, onReset, isDraw }) {
+  const shouldEnabledButton = () =>  winner !== 0 || isDraw;
 
   return (
     <div className={styles.gameInfo}>
-      {winner === 0 && (
+      {
+      !isDraw && winner === 0 && (
         <>
           <h4>Próximo a jogar:</h4>
           {currentPlayer === 1 && <Icon iconName="circle" />}
           {currentPlayer === -1 && <Icon iconName="x" />}
         </>
       )}
-      {winner !== 0 && (
+      {!isDraw && winner !== 0 && (
         <>
           <h4>Fim de Jogo! Campeão:</h4>
           {winner === 1 && <Icon iconName="circle" />}
           {winner === -1 && <Icon iconName="x" />}
         </>
       )}
-      <Button 
-      onClick={onReset} 
-      disabled={!shouldEnabledButton()}>
+      {
+        isDraw && <h4>Empate!</h4>
+      }
+      <Button onClick={onReset} disabled={!shouldEnabledButton()}>
         Reiniciar
       </Button>
     </div>
