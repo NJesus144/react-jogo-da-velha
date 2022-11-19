e06f4976b00ce56508a5e1c7f04bc9df1616f219
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-11-19 17:24:37 -0300

    criado lógica para declarar o campeão e reset

---
+++ src/components/gameInfo/GameInfo.jsx
@@ -0,0 +1,35 @@
+import styles from "./GameInfo.module.css";
+import Icon from "../icon/Icon";
+import Button from "../button/Button";
+
+function GameInfo({ currentPlayer, winner, onReset }) {
+  const shouldEnabledButton = () => {
+    return winner !== 0;
+  };
+
+  return (
+    <div className={styles.gameInfo}>
+      {winner === 0 && (
+        <>
+          <h4>Próximo a jogar:</h4>
+          {currentPlayer === 1 && <Icon iconName="circle" />}
+          {currentPlayer === -1 && <Icon iconName="x" />}
+        </>
+      )}
+      {winner !== 0 && (
+        <>
+          <h4>Fim de Jogo! Campeão:</h4>
+          {winner === 1 && <Icon iconName="circle" />}
+          {winner === -1 && <Icon iconName="x" />}
+        </>
+      )}
+      <Button 
+      onClick={onReset} 
+      disabled={!shouldEnabledButton()}>
+        Reiniciar
+      </Button>
+    </div>
+  );
+}
+
+export default GameInfo;
